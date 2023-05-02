c28a503f3d0f86cf1e5b12c8477f9924b667ba3a
Craig Patterson <craiglp@users.noreply.github.com>
2023-05-01 20:54:40 -0500

    Cleaned up spaces/tabs

--- main.asm
+++ main.asm
@@ -27,32 +27,32 @@
 
 			INCLUDE	"equs.inc"
 			INCLUDE "mos_api.inc"
-			
+
 			SEGMENT CODE
-						
+
 			XDEF	_main
-			
-			SCRMODE		EQU		3h				  		;Screen Mode 3
-			
-			ROWS        EQU     57                		;Rows on screen -2 rows for status line
-			COLS        EQU     79                		;Columns on screen
-			
-			TOT_CELLS	EQU		(ROWS+2)*(COLS+1)+1		;Total number of cells
 
-			CURRBASE    EQU     _MATRIX_START      		;Base address of Cell primary cell table
-			CURRSTART   EQU     CURRBASE+COLS+2   		;Primary start position Base+42
+			SCRMODE		EQU		3h							;Screen Mode 3
 
-			NEXTBASE    EQU     CURRBASE+TOT_CELLS+1	;Base address of Cell secondary cell table
-			NEXTSTART   EQU     NEXTBASE+COLS+2   		;Secondary start position Base+COLS+2
-			
-            UPPER_LEFT		EQU		COLS+2				; Look upper left (-42 cells)
-            UPPER_MID		EQU		COLS+1				; Look upper mid (-41 cells)
-            UPPER_RIGHT		EQU		COLS				; Look upper right (-40 cells)
-            MID_LEFT		EQU		1					; Look mid left (-1 cell)
-            MID_RIGHT		EQU		1					; Look mid right (+1 cell)
-            BOTTOM_LEFT		EQU		COLS				; Look bottom left (+40 cells)
-            BOTTOM_MID		EQU		COLS+1				; Look bottom mid (+41 cells)
-            BOTTOM_RIGHT	EQU		COLS+2				; Look bottom right (+42 cells)
+			ROWS			EQU		57						;Rows on screen -2 rows for status line
+			COLS			EQU		79						;Columns on screen
+
+			TOT_CELLS		EQU		(ROWS+2)*(COLS+1)+1		;Total number of cells
+
+			CURRBASE		EQU		_MATRIX_START			;Base address of Cell primary cell table
+			CURRSTART		EQU		CURRBASE+COLS+2			;Primary start position Base+42
+
+			NEXTBASE		EQU		CURRBASE+TOT_CELLS+1	;Base address of Cell secondary cell table
+			NEXTSTART		EQU		NEXTBASE+COLS+2			;Secondary start position Base+COLS+2
+
+			UPPER_LEFT		EQU		COLS+2
+			UPPER_MID		EQU		COLS+1
+			UPPER_RIGHT		EQU		COLS
+			MID_LEFT		EQU		1
+			MID_RIGHT		EQU		1
+			BOTTOM_LEFT		EQU		COLS
+			BOTTOM_MID		EQU		COLS+1
+			BOTTOM_RIGHT	EQU		COLS+2
 
 
 ;To work out top/bottom cells, I place a zero row one above and below the 1000 cell table. To
@@ -118,45 +118,42 @@ _main:
 			RST.LIL	10h
 
 START:
-            LD      HL,CURRBASE     ;Clear Current Cell data location to be all zeros
-            LD      DE,CURRBASE+1 
-            XOR     A               ;Set to Zero
-            LD      (HL),A 
-            LD      BC,TOT_CELLS    ;Cells to be cleared
-            LDIR                    ;Do the Copy
-            LD      HL,NEXTBASE     ;Clear Next Cell data location to be all zeros
-            LD      DE,NEXTBASE+1 
-            XOR     A               ;Set to Zero
-            LD      (HL),A 
-            LD      BC,TOT_CELLS    ;1108 cells to be cleared
-            LDIR                    ;Do the Copy
-			
-NEWCELLS:            
-            CALL    LOAD_RANDOM		;Initialize cell data with random values
+			LD		HL,CURRBASE     ;Clear Current Cell data location to be all zeros
+			LD		DE,CURRBASE+1 
+			XOR		A               ;Set to Zero
+			LD		(HL),A 
+			LD		BC,TOT_CELLS    ;Cells to be cleared
+			LDIR					;Do the Copy
+			LD		HL,NEXTBASE		;Clear Next Cell data location to be all zeros
+			LD		DE,NEXTBASE+1
+			XOR		A				;Set to Zero
+			LD		(HL),A 
+			LD		BC,TOT_CELLS	;1108 cells to be cleared
+			LDIR					;Do the Copy
+
+NEWCELLS:
+			CALL	LOAD_RANDOM		;Initialize cell data with random values
 
 LIFE:
-			CALL	PRINT_CELLS
-			;CALL    DISPLAY_CELLS	;Fill screen with current cells
-			
-			
+			CALL	PRINT_CELLS		
 			CALL	PRINT_STATUSLINE
 			
-            CALL    CONWAY			;Do Conway Rules on current cells
+			CALL	CONWAY			;Do Conway Rules on current cells
 			
 			;Loop until key pressed			
 			MOSCALL	mos_getkey
 			OR		A 		
 			JR		Z, LIFE
 			
-            CP      ESC				;Escape pressed?
-            JR      NZ,LIFE			;Key pressed but not Escape, reload new random cells
+			CP		ESC				;Escape pressed?
+			JR		NZ,LIFE			;Key pressed but not Escape, reload new random cells
 
 			LD		HL, s_LIFE_END	;Escape pressed, clean up and exit
 			LD		BC, 0
 			LD		A, 0
 			RST.LIS	18h
 											
-	FINI:	
+	FINI:
 			;Enable text cursor
 			LD		A, 23		;VDU 23
 			RST.LIL	10h
@@ -175,81 +172,81 @@ LIFE:
 ;    * Any dead cell with three live neighbours becomes a live cell.
 ;    * All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 CONWAY:
-            LD      IX,CURRSTART
-            LD      HL,NEXTSTART
-            LD      B,ROWS
-	NEWROW:              
-            PUSH    BC ;Save Registers
-            LD      B,COLS ;40 Columns
-	NEWCOL:              
-            ;Check the current cell and update count on number of live cells.  Use IX to
-            ;make checking easier
-            LD      A,(IX-UPPER_LEFT)            ; Look upper left (-42 cells)
-            ADD     A,(IX-UPPER_MID)            ; Look upper mid (-41 cells)
-            ADD     A,(IX-UPPER_RIGHT)            ; Look upper right (-40 cells)
-            ADD     A,(IX-MID_LEFT)             ; Look mid left (-1 cell)
-            ADD     A,(IX+MID_RIGHT)             ; Look mid right (+1 cell)
-            ADD     A,(IX+BOTTOM_LEFT)            ; Look bottom left (+40 cells)
-            ADD     A,(IX+BOTTOM_MID)            ; Look bottom mid (+41 cells)
-            ADD     A,(IX+BOTTOM_RIGHT)            ; Look bottom right (+42 cells)
-			
-		EVALUATE:       
-            ;Evaluate surrounding cell count to create or destroy current cell
-            ;This is the rules being applied for the cell
-            LD      D,01h                ;Alive
-            CP      03h                  ;Check if 3 cells around
-            JR      Z,STOREC             ;Save Alive cell	
-            LD      D,00h                ;Dead
-            CP      02h                  ;Check if 2 cells around
-            JR      NZ,STOREC            ;Save Dead cell if not 2
-            LD      A,(IX+0)             ;Current Cell had only 2 cells around
-            AND     01h                  ;Keep it alive if already alive.
-            LD      D,A                  ;Load current cell in A
-			
-		STOREC:              
-            ;Save the new cell to the Next Cell Cycle table
-            LD      A,D                  ;D stores cell evaluation
-            LD      (HL),A               ;Update cell on Next Matrix
-            INC     HL                   ;Move to next Column
-            INC     IX                   ;Move to next Column
-            DJNZ    NEWCOL               ;Repeat until all columns are checked
+			LD		IX,CURRSTART
+			LD		HL,NEXTSTART
+			LD		B,ROWS
+	NEWROW:
+			PUSH	BC					;Save Registers
+			LD		B,COLS
+	NEWCOL:
+			;Check the current cell and update count on number of live cells.  Use IX to
+			;make checking easier
+			LD		A,(IX-UPPER_LEFT)
+			ADD		A,(IX-UPPER_MID)
+			ADD		A,(IX-UPPER_RIGHT)
+			ADD		A,(IX-MID_LEFT)
+			ADD		A,(IX+MID_RIGHT)
+			ADD		A,(IX+BOTTOM_LEFT)
+			ADD		A,(IX+BOTTOM_MID)
+			ADD		A,(IX+BOTTOM_RIGHT)
 
-            INC     HL                   ;Skip left zero buffer
-            INC     IX                   ;Skip left zero buffer
-            POP     BC 
-            DJNZ    NEWROW               ;Repeat until all rows are checked
+		EVALUATE:
+			;Evaluate surrounding cell count to create or destroy current cell
+			;This is the rules being applied for the cell
+			LD		D,01h					;Alive
+			CP		03h						;Check if 3 cells around
+			JR		Z,STOREC				;Save Alive cell	
+			LD		D,00h					;Dead
+			CP		02h						;Check if 2 cells around
+			JR		NZ,STOREC				;Save Dead cell if not 2
+			LD		A,(IX+0)				;Current Cell had only 2 cells around
+			AND		01h						;Keep it alive if already alive.
+			LD		D,A						;Load current cell in A
 
-            ;Copy next matrix to current
-            LD      HL,NEXTSTART 
-            LD      DE,CURRSTART 
-            LD      BC,TOT_CELLS         ;All cells (include left zero buffer)
-            LDIR                         ;Do the Copy
+		STOREC:
+			;Save the new cell to the Next Cell Cycle table
+			LD		A,D						;D stores cell evaluation
+			LD		(HL),A					;Update cell on Next Matrix
+			INC		HL						;Move to next Column
+			INC		IX						;Move to next Column
+			DJNZ	NEWCOL					;Repeat until all columns are checked
 
-            RET                          ;Exit
+			INC		HL						;Skip left zero buffer
+			INC		IX						;Skip left zero buffer
+			POP		BC 
+			DJNZ	NEWROW					;Repeat until all rows are checked
+
+			;Copy next matrix to current
+			LD		HL,NEXTSTART
+			LD		DE,CURRSTART
+			LD		BC,TOT_CELLS			;All cells (include left zero buffer)
+			LDIR							;Do the Copy
+
+			RET								;Exit
 			
 
 			;Load Random cells in memory.  This interates through all cells and calls
-            ;an psuedo random routine.  If that routine sets the carry flag then set the 
-            ;cell to Alive.
+			;an psuedo random routine.  If that routine sets the carry flag then set the 
+			;cell to Alive.
 LOAD_RANDOM:
-			LD      HL,CURRSTART 
-            LD      B,ROWS 
+			LD		HL,CURRSTART 
+			LD		B,ROWS 
 	COL:									;Columns
-            PUSH    BC 
-            LD      B,COLS 
+			PUSH	BC 
+			LD		B,COLS 
 	ROW:								;Rows
-            CALL    RAND_8				;Call random routine
-            LD      A,01h				;Default to Alive
-            JR      C,STORECELL 
-            XOR     A					;Set to Dead
-	STORECELL:           
-            LD      (HL),A				;Store the cell
-            INC     HL 
-            DJNZ    ROW 
-            INC     HL					;Skip left hand zero column
-            POP     BC 
-            DJNZ    COL 
-            RET							;Exit
+			CALL	RAND_8				;Call random routine
+			LD		A,01h				;Default to Alive
+			JR		C,STORECELL 
+			XOR		A					;Set to Dead
+	STORECELL:
+			LD		(HL),A				;Store the cell
+			INC		HL 
+			DJNZ	ROW 
+			INC		HL					;Skip left hand zero column
+			POP		BC 
+			DJNZ	COL 
+			RET							;Exit
 
 PRINT_CELLS:
 			LD		A,31			;Home text cursor
@@ -264,10 +261,11 @@ PRINT_CELLS:
 	col_loop:
 			PUSH	BC
 			LD		B,COLS
+			
 	row_loop:
 			LD		A,(IX)
 			CALL	Print_Cell
-			
+		
 			INC		IX
 			DJNZ	row_loop
 
@@ -291,7 +289,7 @@ Print_Cell:
 			LD A, 130
 	$$:		RST.LIS 10h
 			LD A,C
-			RET
+			RET							;Exit
 			
 PRINT_STATUSLINE:
 			LD		A, 31				;Move cursor to status line
@@ -310,7 +308,7 @@ PRINT_STATUSLINE:
 			INC		A
 			LD		(GENERATION),A
 			
-			RET
+			RET							;Exit
 			
 ; Returns pseudo random 8 bit number in A. Only affects A.
 ; (r_seed) is the byte from which the number is generated and MUST be
@@ -326,7 +324,7 @@ RAND_8:
 			LD	A,(r_seed)	; get seed back
 			RLA				; rotate carry into byte
 			LD	(r_seed),A	; save back for next prn
-			RET				; done
+			RET				;Exit
 	r_seed:
 			DB	254			; prng seed byte (must not be zero)
 	
@@ -347,28 +345,28 @@ Print_Hex8:
 			ADC		A,40h
 			DAA
 			RST.LIS	10h
-			RET
+			RET							;Exit
 			
 Print_String:
 			LD		BC, 0
 			LD		A, 0
 			RST.LIS	18h
-			RET
+			RET							;Exit
 
-Print_Decimal:	
-; Print a decimal number (less than 1000000)';
-;
+; Print a decimal number (less than 1000000)
 ; Input: HL 24-bit number to print.
+Print_Decimal:	
 			PUSH	IY
 			PUSH	DE
 			PUSH 	BC
 			LD	IY, Num_Table
 			LD	B, 6		; Consider 6 digits
 			LD	C, 6		; Leading zero counter, reaches 1 if sixth digit sill 0.		; 
-Print_Dec1:	LD	DE, (IY+0) 	; Take next power of 10.
+	Print_Dec1:
+			LD	DE, (IY+0) 	; Take next power of 10.
 			LEA	IY, IY+4
 			LD	A, '0'-1
-$$:			INC	A
+	$$:		INC	A
 			AND 	A	
 			SBC 	HL, DE		; Repeatedly subtract power of 10 until carry
 			JR	NC, $B
@@ -377,13 +375,14 @@ $$:			INC	A
 			JR	NZ, $F		; Don't print leading zero			
 			DEC	C	
 			JR	NZ, Print_Dec2	; But do print 0 if it's the units digit.
-$$:			RST.LIL	10h
+	$$:		RST.LIL	10h
 			LD	C, 1		; Make sure the next digit will be printed.
-Print_Dec2:		DJNZ	Print_Dec1
+	Print_Dec2:
+			DJNZ	Print_Dec1
 			POP 	BC
 			POP	DE
 			POP	IY
-			RET
+			RET							;Exit
 
 Num_Table	DL	100000
 			DL	10000
@@ -391,7 +390,6 @@ Num_Table	DL	100000
 			DL	100
 			DL	10
 			DL	1
-
 
 ; Text strings
 ;
@@ -401,7 +399,7 @@ s_cr:		DB	"\r", 0
 s_HOME		DB	30,0,0
 s_STATUS	DB	"Generation: ", 0
 
-s_CELL_CHAR DB	23,130,3Ch,7Eh,FFh,FFh,FFh,FFh,7Eh,3Ch
+s_CELL_CHAR DB	23,130,3Ch,7Eh,C3h,DBh,DBh,C3h,7Eh,3Ch
 
 GENERATION	DB	0
 
